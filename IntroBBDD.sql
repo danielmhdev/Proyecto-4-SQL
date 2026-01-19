0385fe0c071f94c120a3031c2fa6b60e333ef25e
Daniel Martín Hernández <daniel@Mac.home>
2026-01-19 19:46:49 +0100

    SQL terminado ejercicios 1-9

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -1,3 +1,28 @@
 -- 1. Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
 SELECT flight_no, flight_id, status FROM flights WHERE status = 'On Time';
 
+/* 2. Escribe una consulta que extraiga todas las columnas de la tabla bookings y 
+refleje todas las reservas que han supuesto una cantidad total mayor a 1.000.000 (Unidades monetarias).*/
+SELECT * FROM bookings WHERE total_amount > 1000000;
+
+/* 3. Escribe una consulta que extraiga todas las columnas de los datos de los modelos de aviones disponibles (aircraft_data). Puede que os aparezca en alguna actualización como "aircrafts_data", revisad las tablas y elegid la que corresponda. */
+SELECT * FROM aircrafts_data;
+
+/* 4. Con el resultado anterior visualizado previamente, escribe una consulta que extraiga los identificadores de vuelo que han volado con un Boeing 737. (Código Modelo Avión = 733) */
+SELECT flight_id FROM flights WHERE aircraft_code = '733';
+
+/* 5. Escribe una consulta que te muestre la información detallada de los tickets que han comprado las personas que se llaman Irina. */
+SELECT * FROM tickets WHERE passenger_name ILIKE '%Irina%';
+
+/* 6. Mostrar las ciudades con más de un aeropuerto. */
+SELECT city, COUNT(*) AS num_airports FROM airports_data GROUP BY city HAVING COUNT(*) >1;
+
+/* 7. Mostrar el número de vuelos por modelo de avión. */
+SELECT m.model, COUNT(*) AS num_vuelos FROM flights f JOIN aircrafts_data m ON f.aircraft_code = m.aircraft_code GROUP BY m.model;
+
+/* 8. Reservas con más de un billete (varios pasajeros). */
+
+SELECT book_ref, COUNT(*) AS num_tickets FROM tickets GROUP BY book_ref HAVING COUNT(*) >1; 
+
+/* 9. Vuelos con retraso de salida superior a una hora. */
+SELECT flight_id FROM flights WHERE (actual_departure - scheduled_departure) > interval '1 hour';

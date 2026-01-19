10c28a1597ac4c9075534ab4edff6ceeab81edcd
Daniel Martín Hernández <daniel@Mac.home>
2026-01-19 18:50:54 +0100

    primer ejercicio

---
+++ IntroBBDD.sql
@@ -0,0 +1,3 @@
+-- 1. Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
+SELECT flight_no, flight_id, status FROM flights WHERE status = 'On Time';
+
